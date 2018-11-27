9155ff6b85d0bf3a806d5797e7876747d52510ac
ALBERT Louis <albertlo@mac12-m102.utt.fr>
2018-11-27 18:14:37 +0100

    Initial Commit

---
+++ juste nombreTests/juste_nombreTests.swift
@@ -0,0 +1,36 @@
+//
+//  juste_nombreTests.swift
+//  juste nombreTests
+//
+//  Created by ALBERT Louis on 27/11/2018.
+//  Copyright © 2018 ALBERT Louis. All rights reserved.
+//
+
+import XCTest
+@testable import juste_nombre
+
+class juste_nombreTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
